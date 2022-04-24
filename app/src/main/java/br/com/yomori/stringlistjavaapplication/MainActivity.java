d3c4336391b45d5d66f920103ecc69072dc2c2c4
Yoshio Mori <yoshio.mori@hotmail.com>
2022-04-24 14:38:14 -0300

    Refactored code: clear unused imports

--- app/src/main/java/br/com/yomori/stringlistjavaapplication/MainActivity.java
+++ app/src/main/java/br/com/yomori/stringlistjavaapplication/MainActivity.java
@@ -1,13 +1,11 @@
 package br.com.yomori.stringlistjavaapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
