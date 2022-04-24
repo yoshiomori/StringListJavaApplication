1c6dca5f8b1dabdabcec6e13855c3cf6f23d47fe
Yoshio Mori <yoshio.mori@hotmail.com>
2022-04-24 11:25:46 -0300

    Initial commit

---
+++ app/src/main/java/br/com/yomori/stringlistjavaapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package br.com.yomori.stringlistjavaapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView citiesList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        citiesList = findViewById(R.id.citiesList);
+
+        ArrayList<String> cities = new ArrayList<>();
+        cities.add("Zurich");
+        cities.add("New York");
+        cities.add("Berlin");
+        cities.add("Moscow");
+        cities.add("Madrid");
+
+        ArrayAdapter<String> citiesAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                cities
+        );
+
+        citiesList.setAdapter(citiesAdapter);
+    }
+}
